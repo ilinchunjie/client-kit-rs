be0d9591bf04bd740465c007b2edeac7ce25c5fb
林纯杰 <linchunjie@xmfunny.com>
2024-01-24 15:13:25 +0800

    update clientkit

--- src/download/download_extern.rs
+++ src/download/download_extern.rs
@@ -32,13 +32,15 @@ pub extern "C" fn start_download_service(multi_thread: bool, thread_count: u16, 
 }
 
 #[no_mangle]
-pub extern "C" fn set_parallel_count(ptr: *mut DownloadService, count: u16) {
+pub extern "C" fn set_parallel_count(ptr: *mut FFIDownloadService, count: u16) {
+    let ptr = ptr as *mut DownloadService;
     let service = unsafe { ptr.as_mut().expect("invalid ptr: ") };
     service.set_parallel_count(count as usize)
 }
 
 #[no_mangle]
-pub extern "C" fn stop_download_service(ptr: *mut DownloadService) {
+pub extern "C" fn stop_download_service(ptr: *mut FFIDownloadService) {
+    let ptr = ptr as *mut DownloadService;
     if ptr.is_null() {
         return;
     }
@@ -49,7 +51,8 @@ pub extern "C" fn stop_download_service(ptr: *mut DownloadService) {
 }
 
 #[no_mangle]
-pub extern "C" fn add_downloader(ptr: *mut DownloadService, config: DownloadConfig) -> *mut FFIDownloadOperation {
+pub extern "C" fn add_downloader(ptr: *mut FFIDownloadService, config: DownloadConfig) -> *mut FFIDownloadOperation {
+    let ptr = ptr as *mut DownloadService;
     let download_service = unsafe { ptr.as_mut().expect("invalid ptr: ") };
     let url = config.url.to_string();
     let path = config.path.to_string();
@@ -67,25 +70,29 @@ pub extern "C" fn add_downloader(ptr: *mut DownloadService, config: DownloadConf
 }
 
 #[no_mangle]
-pub extern "C" fn get_download_status(ptr: *mut DownloadOperation) -> u8 {
+pub extern "C" fn get_download_status(ptr: *mut FFIDownloadOperation) -> u8 {
+    let ptr = ptr as *mut DownloadOperation;
     let operation = unsafe { ptr.as_mut().expect("invalid ptr: ") };
     operation.status().into()
 }
 
 #[no_mangle]
-pub extern "C" fn get_download_is_done(ptr: *mut DownloadOperation) -> bool {
+pub extern "C" fn get_download_is_done(ptr: *mut FFIDownloadOperation) -> bool {
+    let ptr = ptr as *mut DownloadOperation;
     let operation = unsafe { ptr.as_mut().expect("invalid ptr: ") };
     operation.is_done()
 }
 
 #[no_mangle]
-pub extern "C" fn get_download_is_error(ptr: *mut DownloadOperation) -> bool {
+pub extern "C" fn get_download_is_error(ptr: *mut FFIDownloadOperation) -> bool {
+    let ptr = ptr as *mut DownloadOperation;
     let operation = unsafe { ptr.as_mut().expect("invalid ptr: ") };
     operation.is_error()
 }
 
 #[no_mangle]
-pub extern "C" fn get_download_error(ptr: *mut DownloadOperation) -> *mut FFIString {
+pub extern "C" fn get_download_error(ptr: *mut FFIDownloadOperation) -> *mut FFIString {
+    let ptr = ptr as *mut DownloadOperation;
     let operation = unsafe { ptr.as_mut().expect("invalid ptr: ") };
     let error = operation.error().to_string().into();
     Box::into_raw(Box::new(error))
@@ -102,25 +109,29 @@ pub extern "C" fn free_download_error(ptr: *mut FFIString) {
 }
 
 #[no_mangle]
-pub extern "C" fn get_downloaded_size(ptr: *mut DownloadOperation) -> u64 {
+pub extern "C" fn get_downloaded_size(ptr: *mut FFIDownloadOperation) -> u64 {
+    let ptr = ptr as *mut DownloadOperation;
     let operation = unsafe { ptr.as_mut().expect("invalid ptr: ") };
     operation.downloaded_size()
 }
 
 #[no_mangle]
-pub extern "C" fn get_download_progress(ptr: *mut DownloadOperation) -> f64 {
+pub extern "C" fn get_download_progress(ptr: *mut FFIDownloadOperation) -> f64 {
+    let ptr = ptr as *mut DownloadOperation;
     let operation = unsafe { ptr.as_mut().expect("invalid ptr: ") };
     operation.progress()
 }
 
 #[no_mangle]
-pub extern "C" fn stop_downloader(ptr: *mut DownloadOperation) {
+pub extern "C" fn stop_downloader(ptr: *mut FFIDownloadOperation) {
+    let ptr = ptr as *mut DownloadOperation;
     let operation = unsafe { ptr.as_mut().expect("invalid ptr: ") };
     operation.stop()
 }
 
 #[no_mangle]
-pub extern "C" fn downloader_dispose(ptr: *mut DownloadOperation) {
+pub extern "C" fn downloader_dispose(ptr: *mut FFIDownloadOperation) {
+    let ptr = ptr as *mut DownloadOperation;
     if ptr.is_null() {
         return;
     }
