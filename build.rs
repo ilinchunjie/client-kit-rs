9b7bd923773b0c96e3219033874b2576495a308a
林纯杰 <linchunjie@xmfunny.com>
2024-05-23 15:10:55 +0800

    <feature> support unity debug

--- build.rs
+++ build.rs
@@ -86,4 +86,18 @@ fn main() {
         .generate_csharp_file("csharp/ZipArchive/ZipFileWrap.cs")
         .unwrap();
     // vfs
+
+    //debug
+    csbindgen::Builder::default()
+        .csharp_class_accessibility("internal")
+        .csharp_class_name("DebugWrap")
+        .csharp_namespace("URS.ClientKit")
+        .csharp_dll_name("clientkit")
+        .csharp_dll_name_if("UNITY_IOS && !UNITY_EDITOR", "__Internal")
+        .csharp_entry_point_prefix("")
+        .csharp_method_prefix("")
+        .input_extern_file("src/unity_debug/debug_extern.rs")
+        .generate_csharp_file("csharp/Debug/DebugWrap.cs")
+        .unwrap();
+    //debug
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod download;
 pub mod vfs;
 pub mod ffi;
 pub mod byte_buffer;
+pub mod unity_debug;
